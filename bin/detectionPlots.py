6a79edceea92da50069ca849970096c71382f61a
Conor Sayres <csayres@uw.edu>
2016-03-31 21:54:52 -0700

    recursive diving script for generating centroid plots

---
+++ bin/detectionPlots.py
@@ -0,0 +1,29 @@
+"""Routines for processing images from the lco mapper
+"""
+from __future__ import division, absolute_import
+
+import traceback
+import os
+
+from pymapper.imgProcess import sortDetections
+from pymapper.camera import unpickleCentroids
+
+MINCOUNTS = 0
+MINSEP = 3.5 # min between fibers separation in pixels
+
+def findRawImgDirs(self):
+    rawImgDirs = []
+    for aDir in os.walk("/home/mapper/scan"):
+        if "rawImage" in aDir[0]:
+            rawImgDirs.append(aDir[0])
+    return rawImgDirs
+
+
+if __name__ == "__main__":
+    imgDirs = findRawImgDirs()
+    for imgDir in imgDirs:
+        print("on dir", imgDir)
+        try:
+            sortDetections(unpickleCentroids(imgDir), plot=True)
+        except:
+            traceback.print_exc()
