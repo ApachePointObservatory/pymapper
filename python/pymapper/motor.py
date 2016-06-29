6f7069eb83e8b1b53e11a7a031fdc5483f548a78
Conor Sayres <csayres@uw.edu>
2016-06-29 15:28:33 -0700

    add command line interface, create logs

--- python/pymapper/motor.py
+++ python/pymapper/motor.py
@@ -2,6 +2,8 @@
 """
 from __future__ import division, absolute_import
 
+import logging
+
 from twisted.internet.protocol import Protocol, ClientFactory
 from twisted.internet.endpoints import TCP4ClientEndpoint
 from twisted.internet import reactor
@@ -29,7 +31,7 @@ class Command(object):
     def setDone(self):
         if self.isDone:
             raise RuntimeError("cannot set command %s done, already done!"%self.cmdStr)
-        print("setting", self.cmdStr, "done!")
+        logging.info("setting", self.cmdStr, "done!")
         self.isDone = True
         for func in self.callFuncs:
             func()
@@ -55,24 +57,24 @@ class MotorProtocol(Protocol):
     def connectionMade(self):
         """Called when a connection is made
         """
-        print("connection made")
+        logging.info("connection made")
 
 class MotorClientFactory(ClientFactory):
     def __init__(self, motorControllerInstance):
         self.mci = motorControllerInstance
 
     def startedConnecting(self, connector):
-        print("Started to connect to motor.")
+        logging.info("Started to connect to motor.")
 
     def buildProtocol(self, addr):
-        print("Connected to motor.")
+        logging.info("Connected to motor.")
         return MotorProtocol(self.mci)
 
     # def clientConnectionLost(self, connector, reason):
-    #     print("Lost connection to motor.  Reason:", reason)
+    #     logging.info("Lost connection to motor.  Reason:", reason)
 
     # def clientConnectionFailed(self, connector, reason):
-    #     print("Connection failed!")
+    #     logging.info("Connection failed!")
         #raise RuntimeError("Connection to motor failed. Reason:%s"%reason)
 
 # class MotorStatus(object):
@@ -84,11 +86,23 @@ class MotorClientFactory(ClientFactory):
 #         self.laserOn = None
 
 class MotorController(object):
-    def __init__(self, readyCallback=None, hostname=HOSTNAME, port=PORT):
+    def __init__(self,
+        readyCallback=None,
+        startPos=STARTPOS,
+        endPos=ENDPOS,
+        scanSpeed=SCANSPEED,
+        quickSpeed = QUICKSPEED,
+        hostname=HOSTNAME,
+        port=PORT
+        ):
         """readyCallback called when MotorController is ready to scan!
         """
         self.hostname = HOSTNAME
         self.port = port
+        self.startPos = startPos
+        self.endPos = endPos
+        self.scanSpeed = scanSpeed
+        self.quickSpeed = quickSpeed
         self.readyCallback = readyCallback
         # self.status = MotorStatus()
         self.mcf = MotorClientFactory(self)
@@ -113,13 +127,13 @@ class MotorController(object):
         connDeferred.addErrback(self.connFailed)
 
     def disconnect(self):
-        print("disconnecting from ASCII server")
+        logging.info("disconnecting from ASCII server")
         return self.protocol.transport.loseConnection()
-        print("killing twisted event loop")
+        # logging.info("killing twisted event loop")
         # reactor.stop()
 
     def connFailed(self, failure):
-        print("conn failed errback")
+        logging.info("conn failed errback")
         reactor.stop()
         # raise RuntimeError("conn failed", str(failure))
 
@@ -127,68 +141,68 @@ class MotorController(object):
         self.protocol = protocol
 
     def prepareToScan(self, foo):
-        print("preparing for scan")
+        logging.info("preparing for scan")
         # foo is ignored arg passed via callback framework
         # could send a stop first...
         self.getStatus(callFunc=self.checkHomeThenMove)
 
     def scan(self, callFunc=None):
-        self.move(ENDPOS)
+        self.move(self.endPos)
         self.laserOff(callFunc=callFunc)
         # send motor back to start position
         self.resetAfterScan()
 
     def resetAfterScan(self):
-        print("resetAfterScan")
+        logging.info("resetAfterScan")
         # foo is ignored arg passed via callback framework
         # could send a stop first...
         # self.getStatus(callFunc=self.checkHomeThenMove)
         self.setSpeed(QUICKSPEED)
-        self.move(STARTPOS, callFunc=self.disconnect)
+        self.move(self.startPos, callFunc=self.disconnect)
 
     def checkHomeThenMove(self):
         if not self.isHomed:
-            print("Slit Head Axis is not homed.  Home it before proceeding!")
+            logging.info("Slit Head Axis is not homed.  Home it before proceeding!")
             reactor.stop()
             # raise RuntimeError("Slit Head Axis is not homed.  Home it before proceeding!")
         else:
-            print("Axis is Homed!!")
+            logging.info("Axis is Homed!!")
             # move motor in position for scan.
-            self.setSpeed(QUICKSPEED)
-            self.move(STARTPOS)
-            self.setSpeed(SCANSPEED)
+            self.setSpeed(self.quickSpeed)
+            self.move(self.startPos)
+            self.setSpeed(self.scanSpeed)
             self.laserOn(callFunc=self.readyCallback)
 
     def getStatus(self, callFunc=None):
-        # print("getStatus")
+        logging.info("getStatus")
         return self.queueCommand("status", callFunc=callFunc)
 
     def setSpeed(self, value, callFunc=None):
-        # print("set speed to %.2f"%float(value))
+        logging.info("set speed to %.2f"%float(value))
         return self.queueCommand("speed %.2f"%float(value), callFunc=callFunc)
 
     def move(self, value, callFunc=None):
-        # print("move to %.2f"%float(value))
+        logging.info("move to %.2f"%float(value))
         return self.queueCommand("move %.2f"%float(value), callFunc=callFunc)
 
     def laserOn(self, callFunc=None):
-        # print("laser on")
+        logging.info("laser on")
         return self.queueCommand("lonn", callFunc=callFunc)
 
     def laserOff(self, callFunc=None):
-        # print("laser off")
+        logging.info("laser off")
         return self.queueCommand("loff", callFunc=callFunc)
 
     def dataReceived(self, data):
         if self.currCmd is None:
-            print("unsolicited dataReceived: %s"%str(data))
+            logging.info("unsolicited dataReceived: %s"%str(data))
             return # don't do anything with unsolicited output...
         for dataline in data.split("\n"):
             dataline = dataline.strip().lower()
             if not dataline:
                 # ignore blank strings...
                 continue
-            print("laser output:", dataline)
+            logging.info("laser output:", dataline)
             # right now I only care if the axis is homed
             # don't care about managing any other status bits,
             # however add a parser here to keep track of things
@@ -207,11 +221,11 @@ class MotorController(object):
         if not self.currCmd.isDone:
             raise RuntimeError("cannot send %s, currently busy with %s"%(command.cmdStr, self.currCmd.cmdStr))
         self.currCmd = command
-        print("sending: ", command.cmdStr)
+        logging.info("sending: ", command.cmdStr)
         self.protocol.transport.write(command.cmdStr)
 
     def queueCommand(self, cmdStr, callFunc=None):
-        print("queueCommand", cmdStr)
+        logging.info("queueCommand", cmdStr)
         command = Command(cmdStr, callFunc=callFunc)
         command.addCallback(self.runQueue)
         self.commandQueue.append(command)
@@ -232,11 +246,11 @@ if __name__ == "__main__":
     mc = None
     def cleanup():
         global mc
-        print("Cleaning up")
+        logging.info("Cleaning up")
         mc.resetAfterScan()
     def imready():
         global mc
-        print("I'm READY!!!!")
+        logging.info("I'm READY!!!!")
         mc.scan(cleanup)
     mc = MotorController(imready)
     # reactor.callLater(mc.resetAfterScan)
--- python/pymapper/runMapper.py
+++ python/pymapper/runMapper.py
@@ -2,6 +2,8 @@
 """
 from __future__ import division, absolute_import
 
+import argparse
+
 from math import floor
 import os
 import glob
@@ -17,10 +19,51 @@ from pymapper.motor import MotorController
 from pymapper.fiberAssign import FocalSurfaceSolver
 
 homedir = os.path.expanduser("~")
-scandir = os.path.join(homedir, "scan")
-platelistdir = os.environ["PLATELIST_DIR"]
+baseDir = os.path.join(homedir, "Documents/Camera_test")
+# scandir = os.path.join(homedir, "scan")
+baseName = "test"
 
 
+import sys
+import logging
+
+"""
+todo: add re-detect/re-solve options
+"""
+
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+
+    from: http://stackoverflow.com/questions/3041986/python-command-line-yes-no-input
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
 
 def determineScanNumber(plateID, mjddir):
     # replace plPlugMapP-XXX with plPlugMapM-XXX
@@ -37,67 +80,161 @@ def pathPlugMapP(plateID):
     #replace 10s, 1s place with XX
     plateSubDir = plateZfill[:-2] + "XX"
     fileName = "plPlugMapP-%i.par"%plateID
+    platelistdir = os.environ["PLATELIST_DIR"]
     return os.path.join(platelistdir, "plates", plateSubDir, plateZfill, fileName)
+
+def pickleDetectionList(detectionList, scanDir):
+    # save detections to picked file
+    fileName = os.path.join(scanDir, "detectionList.pkl")
+    output = open(fileName, "wb")
+    pickle.dump(detectionList, output)
+    output.close()
 
 
 if __name__ == "__main__":
 
-    operator = str(raw_input("Enter Name(s): "))
-    cartID = int(raw_input("Enter Cart Number: "))
-    plateID = int(raw_input("Enter Plate Number: "))
+    parser = argparse.ArgumentParser(description="Run the plate mapper.")
+    parser.add_argument("plateID", type=int, help="Plate ID")
+    parser.add_argument("--scanDir", required=False, help="""Directory in which to put scan.
+                            If not provided, one will be automatically determined"""
+                            )
+    parser.add_argument("--rootDir", required=False,
+        default=baseDir, help="Root directory, scanDir will be created here.")
+    parser.add_argument("--startPos", required=False, type=float, default=20,
+        help="begin of scan motor position (mm).")
+    parser.add_argument("--endPos", required=False, type=float, default=140,
+        help="end of scan motor position (mm).")
+    parser.add_argument("--scanSpeed", required=False, type=float, default=0.6,
+        help="speed at which motor scans (mm/sec).")
+    parser.add_argument("--makePlots", action="store_true", default=False, help="if present create png plots with circled dectections, takes much longer." )
+    args = parser.parse_args()
+    parser.add_argument("--solve", action="store_true", default=False, help="if present, solve plate matching fibers to holes.")
+    plateID = args.plateID
+    baseDir = os.path.abspath(args.rootDir)
+        # baseDir doesn't exist
+    # verify base directory is ok
+    if not os.path.exists(baseDir):
+        question = "Base directory %s does not exist, create it?"%baseDir
+        output = query_yes_no(question, default="yes")
+        if output:
+            try:
+                os.makedirs(baseDir)
+            except:
+                print("failed to create base directory %s (permissions?)"%baseDir)
+                sys.exit()
+        else:
+            # exit program
+            print("bye!"); sys.exit()
 
-    MJD = floor(datetime.now().mjd)
-    print("MJD: %i"%MJD)
-    #create mjd directory
-    mjddir = os.path.join(scandir, "%i"%MJD)
-    if not os.path.exists(mjddir):
-        os.makedirs(mjddir)
-    plateDir = os.path.join(mjddir, "%i"%plateID)
-    if not os.path.exists(plateDir):
-        os.makedirs(plateDir)
-    # create a scan number dir
-    scanNum = 1
-    while True:
-        imageDir = os.path.join(plateDir, "rawImage-%i"%scanNum)
-        if not os.path.exists(imageDir):
-            os.makedirs(imageDir)
-            break
-        scanNum += 1
-    print("scanNumber %i"%scanNum)
+    if args.scanDir is None:
+        # no scan dir, try to provide the next one
+        # try to determine the next number in the
+        # directory sequence
+        testDirs = sorted(glob.glob(os.path.join(baseDir, "%s*"%baseName)))
+        if testDirs:
+            lastTestDir = testDirs[-1]
+            # grab last three chars (they should be ints)
+            lastTestInt = int(lastTestDir[-3:])
+            nextTestInt = lastTestInt + 1
+            scanDir = "%s%s"%(baseName, ("%i"%nextTestInt).zfill(3))
+        else:
+            # no existing test dirs, create the first one?
+            scanDir = "test001"
+        question = "No scan directory provided, create new one: %s?"%scanDir
+        output = query_yes_no(question, default="yes")
+        if not output:
+            print("bye!"); sys.exit()
+    else:
+        scanDir = args.scanDir
+    scanDir = os.path.join(baseDir, scanDir)
+    if os.path.exists(scanDir):
+        # scan dir already exists, check if images have been written here yet?
+        imgs = glob.glob(os.path.join(scanDir, "*.bmp"))
+        if imgs:
+            # images exist ask user what to do
+            question = "%i pre-existing images in %s, REMOVE ALL?"%(len(imgs), scanDir)
+            output = query_yes_no(question, default="yes")
+            if output:
+                try:
+                    for img in imgs:
+                        os.remove(img)
+                except:
+                    print("failed to remove existing images in %s (permissions?)"%scanDir)
+                    sys.exit()
+    else:
+        #create the new scan dir
+        try:
+            os.makedirs(scanDir)
+        except:
+            print("failed to create scan directory in %s (permissions?)"%scanDir)
+            sys.exit()
+
+    # configure logging
+    logfile = os.path.join(scanDir, "scan.log")
+    if os.path.exists(logfile):
+        os.remove(logfile)
+    logging.basicConfig(filename=logfile, level=logging.DEBUG)
+
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # ch.setFormatter(formatter)
+    root.addHandler(ch)
+
+
+    logging.info("scanDir: %s"%scanDir)
+    logging.info("plate ID: %i"%args.plateID)
+    logging.info("motor start pos (mm): %.2f"%args.startPos)
+    logging.info("motor end pos (mm): %.2f"%args.endPos)
+    logging.info("motor scan speed (mm/sec): %.2f"%args.scanSpeed)
+
+
     # create directory to hold camera images
     # note all previous images will be removed if image dir is not empty
-    camera = Camera(imageDir)
+    camera = Camera(scanDir)
 
     # setup object that finds and holds detections
     # detectedFiberList = DetectedFiberList()
 
     # construct motor, nothing happens until connect is called
-    motorController = MotorController()
+    motorController = MotorController(
+        startPos = args.startPos,
+        endPos = args.endPos,
+        scanSpeed = args.scanSpeed,
+        )
 
     # set up callback chains for mapping process
 
     def stopCamera():
-        print("stopCamera")
+        logging.info("stopCamera")
         # motor is done scanning, kill camera
         camera.stopAcquisition()
 
     def moveMotor():
         # camera is acquiring begin moving motor/laser
-        print("moveMotor")
+        logging.info("moveMotor")
         motorController.scan(callFunc=stopCamera)
 
     def startCamera():
         # motor is ready to move, begin snapping pics
-        print("startCamera")
+        logging.info("startCamera")
         camera.beginAcquisition(callFunc=moveMotor)
 
     def solvePlate():
         # load the (previously pickled centroid list)
-        detectedFiberList = sortDetections(camera.centroidList)
-        plugMapPath = pathPlugMapP(plateID)
-        print("plugmap path", plugMapPath)
-        assert os.path.exists(plugMapPath)
-        fss = FocalSurfaceSolver(detectedFiberList, plugMapPath)
+        logging.info("sorting detections. makePlots=%s"%str(args.makePlots))
+        detectedFiberList = sortDetections(camera.centroidList, plot=args.makePlots)
+        # pickle and save the detection list
+        pickleDetectionList(detectedFiberList, scanDir)
+        # not yet ready to solve plate:
+
+        # plugMapPath = pathPlugMapP(plateID)
+        # print("plugmap path", plugMapPath)
+        # assert os.path.exists(plugMapPath)
+        # fss = FocalSurfaceSolver(detectedFiberList, plugMapPath)
 
     camera.doneProcessingCallback(solvePlate)
 
@@ -117,4 +254,23 @@ if __name__ == "__main__":
 
 
 
-
+"""
+    MJD = floor(datetime.now().mjd)
+    print("MJD: %i"%MJD)
+    #create mjd directory
+    mjddir = os.path.join(scandir, "%i"%MJD)
+    if not os.path.exists(mjddir):
+        os.makedirs(mjddir)
+    plateDir = os.path.join(mjddir, "%i"%plateID)
+    if not os.path.exists(plateDir):
+        os.makedirs(plateDir)
+    # create a scan number dir
+    scanNum = 1
+    while True:
+        imageDir = os.path.join(plateDir, "rawImage-%i"%scanNum)
+        if not os.path.exists(imageDir):
+            os.makedirs(imageDir)
+            break
+        scanNum += 1
+    print("scanNumber %i"%scanNum)
+"""
