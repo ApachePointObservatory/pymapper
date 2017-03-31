ae15ef5928b0d06fef2e974fc8b00c376a9c8064
csayres <csayres@uw.edu>
2017-03-31 12:46:15 +0000

    move and compress missing files from scan

--- bin/moveAndCompress.py
+++ bin/moveAndCompress.py
@@ -3,30 +3,32 @@ import shutil
 import subprocess
 import glob
 
-fromDirBase = "/data/savedLCOMapScans/"
+fromDirBase = "/home/lcomapper/scan/"
 toDirBase = "/data/rawmapper/"
-#walkDirs = os.walk(fromDirBase)
-#endDirs = {}
-#for path, dirs, files in walkDirs:
-#    continue # already done
-#    if "fscan" in path:
-#        tailDir = path.split(fromDirBase)[-1]
-#        toDir = os.path.join(toDirBase, tailDir)
-#        if not os.path.exists(toDir):
-#            print("creating", toDir)
-#            os.makedirs(toDir)
-#        for f in files:
-#            fromFile = os.path.join(path, f)
-#            toFile = os.path.join(toDir, f)
-#            if not os.path.exists(toFile):
-#                print("creating file", toFile)
-#                shutil.copyfile(fromFile, toFile)
-
-print("beginning to compress files via fpack")
-for path, dirs, files in os.walk(toDirBase):
+walkDirs = os.walk(fromDirBase)
+endDirs = {}
+for path, dirs, files in walkDirs:
     if "fscan" in path:
-        if glob.glob(os.path.join(path, "*.fits")):
-            # uncompressed fits files exist
-            print("compressing images in ", path)
-            p = subprocess.Popen("fpack -D *.fits", cwd=path, shell=True)
+        tailDir = path.split(fromDirBase)[-1]
+        toDir = os.path.join(toDirBase, tailDir)
+        if not os.path.exists(toDir):
+            print("creating", toDir)
+            os.makedirs(toDir)
+            for f in files:
+                fromFile = os.path.join(path, f)
+                toFile = os.path.join(toDir, f)
+                if not os.path.exists(toFile):
+                    print("creating file", toFile)
+                    shutil.copyfile(fromFile, toFile)
+            # compress the files we just moved
+            print("compressing fits files in %s"%toDir)
+            p = subprocess.Popen("fpack -D *.fits", cwd=toDir, shell=True)
+
+#print("beginning to compress files via fpack")
+#for path, dirs, files in os.walk(toDirBase):
+#    if "fscan" in path:
+#        if glob.glob(os.path.join(path, "*.fits")):
+#            # uncompressed fits files exist
+#            print("compressing images in ", path)
+#            p = subprocess.Popen("fpack -D *.fits", cwd=path, shell=True)
 
